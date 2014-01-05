d2bbf19ffaa6401db7e1e15bf9e0a09a6ab11af9
Carson Gross <carsongross@gmail.com>
2014-01-05 09:15:36 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IntercoolerRailsDemo::Application.config.secret_key_base = 'a59613cf330e97ba1eaedb491563bedd5498ce13be529d0eb9e4da2d19e42ae80383a9426c583f18ddae5f6ae56702262b907b2676b389772f6e5bcf60232066'
