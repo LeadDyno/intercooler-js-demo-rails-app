17306f5369f4ec937d2eae567fce473a553f3f2a
Carson Gross <carsongross@gmail.com>
2014-03-07 15:15:07 -0800

    comments

--- app/controllers/job_controller.rb
+++ app/controllers/job_controller.rb
@@ -6,28 +6,30 @@ class JobController < ApplicationController
 
   def status
     if request.get?
+    # returns the job running UI
       @job = FakeJob.find(session)
       render partial:'status'
     elsif request.post?
+      # starts a new job, lets dependencies take care of refresh
       @job = FakeJob.new(session)
       render nothing: true
     elsif request.delete?
+      # clears the job, lets dependencies take care of refresh
       @job = FakeJob.find(session).clear()
-      render nothing: true
-    else
       render nothing: true
     end
   end
 
   def status_txt
-    @job = FakeJob.find(session)
-    render inline: @job.status
+    # simply returns the current status
+    render inline: FakeJob.find(session).status
   end
 
   def status_percent
     @job = FakeJob.find(session)
+    # If the job is complete, force the outer UI to refresh via a dependency
     response.headers['X-IC-Refresh'] = '/job' if @job.complete?
-    render inline: FakeJob.find(session).percent_str
+    render inline: @job.percent_str
   end
 
 end
